735e5e957ea2ba261ec05929dda800e4be1be5a7
ANGLIA\lc1203 <nomob4321@gmail.com>
2023-11-29 09:03:31 +0000

    Workaround to different player move distances (needs proper fixing), fixed character alignment issues

--- Assets/PlayerScript/PlayerMovement.cs
+++ Assets/PlayerScript/PlayerMovement.cs
@@ -12,6 +12,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void MoveRight()
     {
-        transform.position += new Vector3(0, 0, 1f);
+        transform.position += new Vector3(0, 0, 0.5f); // Temporary hotfix for double trigger on move right
     }
 }
